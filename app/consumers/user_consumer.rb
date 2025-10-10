1c97b62dee3aa6990e18acb519e44862572b4e58
D10-TRUNGPV-PC <phamtrung.tla@gmail.com>
2025-10-10 11:00:44 +0700

    add: user model

--- app/consumers/user_consumer.rb
+++ app/consumers/user_consumer.rb
@@ -11,7 +11,11 @@ class UserConsumer
       Rails.logger.info("Received message: #{data.inspect}")
 
       if data['event'] == 'user_confirmed'
-        puts data
+        user = User.new(id: data["id"], email: data["email"], line_user_id: data["line_user_id"], dob: data["dob"], full_name: data["full_name"])
+        user.save
+      elsif data['event'] == 'user_updated'
+        user = User.find_by(id: data["id"])
+        user.update(email: data["email"], line_user_id: data["line_user_id"], dob: data["dob"], full_name: data["full_name"])
       end
     end
   end
